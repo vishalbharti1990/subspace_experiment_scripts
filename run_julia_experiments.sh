e29f6434a5e7c5434cafaceadd5893e337a3d210
vishalbharti1990 <vbharti@gradcenter.cuny.edu>
2020-12-28 16:51:23 -0500

    updated setup.sh

--- run_julia_experiments.sh
+++ run_julia_experiments.sh
@@ -9,10 +9,10 @@ echo "Running synthetic dataset experiments..."
 # 1. Synthetic datasets
 
 # 1.1 Axis Parallel datasets experiments
-julia $CURR_DIR/Julia_Scripts/run_LMCLUS_axis.jl 100 $CURR_DIR/../subspace_datasets/synthetic_datasets/axis_parallel $CURR_DIR/../Results/synthetic_datasets/axis_parallel/LMCLUS
+julia $CURR_DIR/Julia_scripts/run_LMCLUS_axis.jl 100 $CURR_DIR/../subspace_datasets/synthetic_datasets/axis_parallel $CURR_DIR/../Results/synthetic_datasets/axis_parallel/LMCLUS
 
 # 1.2 Arbitrary datasets experiments
-julia $CURR_DIR/Julia_Scripts/run_LMCLUS_arb.jl 100 $CURR_DIR/../subspace_datasets/synthetic_datasets/arbitrary $CURR_DIR/../Results/synthetic_datasets/arbitrary/LMCLUS
+julia $CURR_DIR/Julia_scripts/run_LMCLUS_arb.jl 100 $CURR_DIR/../subspace_datasets/synthetic_datasets/arbitrary $CURR_DIR/../Results/synthetic_datasets/arbitrary/LMCLUS
 
 echo "Synthetic dataset experiment finished!"
 
@@ -25,36 +25,36 @@ echo "Running real dataset experiments..."
 echo "COIL-100 dataset experiments started..."
 
 # 2.1.1 COIL-100 dataset
-julia $CURR_DIR/Julia_Scripts/run_LMCLUS_image.jl 10 $CURR_DIR/../subspace_datasets/real_datasets/image_datasets/coil-100 vgg16_fc2 $CURR_DIR/../Results/real_datasets/image_datasets/coil-100/LMCLUS
-julia $CURR_DIR/Julia_Scripts/run_LMCLUS_image.jl 10 $CURR_DIR/../subspace_datasets/real_datasets/image_datasets/coil-100 vgg19_fc2 $CURR_DIR/../Results/real_datasets/image_datasets/coil-100/LMCLUS
-julia $CURR_DIR/Julia_Scripts/run_LMCLUS_image.jl 10 $CURR_DIR/../subspace_datasets/real_datasets/image_datasets/coil-100 resnet_avg_pool $CURR_DIR/../Results/real_datasets/image_datasets/coil-100/LMCLUS
-julia $CURR_DIR/Julia_Scripts/run_LMCLUS_image.jl 10 $CURR_DIR/../subspace_datasets/real_datasets/image_datasets/coil-100 xception_avg_pool $CURR_DIR/../Results/real_datasets/image_datasets/coil-100/LMCLUS
-julia $CURR_DIR/Julia_Scripts/run_LMCLUS_image.jl 10 $CURR_DIR/../subspace_datasets/real_datasets/image_datasets/coil-100 inception_avg_pool $CURR_DIR/../Results/real_datasets/image_datasets/coil-100/LMCLUS
-julia $CURR_DIR/Julia_Scripts/run_LMCLUS_image.jl 10 $CURR_DIR/../subspace_datasets/real_datasets/image_datasets/coil-100 efficientnet_avg_pool $CURR_DIR/../Results/real_datasets/image_datasets/coil-100/LMCLUS
+julia $CURR_DIR/Julia_scripts/run_LMCLUS_image.jl 10 $CURR_DIR/../subspace_datasets/real_datasets/image_datasets/coil-100 vgg16_fc2 $CURR_DIR/../Results/real_datasets/image_datasets/coil-100/LMCLUS
+julia $CURR_DIR/Julia_scripts/run_LMCLUS_image.jl 10 $CURR_DIR/../subspace_datasets/real_datasets/image_datasets/coil-100 vgg19_fc2 $CURR_DIR/../Results/real_datasets/image_datasets/coil-100/LMCLUS
+julia $CURR_DIR/Julia_scripts/run_LMCLUS_image.jl 10 $CURR_DIR/../subspace_datasets/real_datasets/image_datasets/coil-100 resnet_avg_pool $CURR_DIR/../Results/real_datasets/image_datasets/coil-100/LMCLUS
+julia $CURR_DIR/Julia_scripts/run_LMCLUS_image.jl 10 $CURR_DIR/../subspace_datasets/real_datasets/image_datasets/coil-100 xception_avg_pool $CURR_DIR/../Results/real_datasets/image_datasets/coil-100/LMCLUS
+julia $CURR_DIR/Julia_scripts/run_LMCLUS_image.jl 10 $CURR_DIR/../subspace_datasets/real_datasets/image_datasets/coil-100 inception_avg_pool $CURR_DIR/../Results/real_datasets/image_datasets/coil-100/LMCLUS
+julia $CURR_DIR/Julia_scripts/run_LMCLUS_image.jl 10 $CURR_DIR/../subspace_datasets/real_datasets/image_datasets/coil-100 efficientnet_avg_pool $CURR_DIR/../Results/real_datasets/image_datasets/coil-100/LMCLUS
 
 echo "COIL-100 dataset experiments finished!"
 
 echo "LEGO dataset experiments started.."
 
 # 2.1.2 lego dataset
-julia $CURR_DIR/Julia_Scripts/run_LMCLUS_image.jl 10 $CURR_DIR/../subspace_datasets/real_datasets/image_datasets/lego vgg16_fc2 $CURR_DIR/../Results/real_datasets/image_datasets/lego/LMCLUS
-julia $CURR_DIR/Julia_Scripts/run_LMCLUS_image.jl 10 $CURR_DIR/../subspace_datasets/real_datasets/image_datasets/lego vgg19_fc2 $CURR_DIR/../Results/real_datasets/image_datasets/lego/LMCLUS
-julia $CURR_DIR/Julia_Scripts/run_LMCLUS_image.jl 10 $CURR_DIR/../subspace_datasets/real_datasets/image_datasets/lego resnet_avg_pool $CURR_DIR/../Results/real_datasets/image_datasets/lego/LMCLUS
-julia $CURR_DIR/Julia_Scripts/run_LMCLUS_image.jl 10 $CURR_DIR/../subspace_datasets/real_datasets/image_datasets/lego xception_avg_pool $CURR_DIR/../Results/real_datasets/image_datasets/lego/LMCLUS
-julia $CURR_DIR/Julia_Scripts/run_LMCLUS_image.jl 10 $CURR_DIR/../subspace_datasets/real_datasets/image_datasets/lego inception_avg_pool $CURR_DIR/../Results/real_datasets/image_datasets/lego/LMCLUS
-julia $CURR_DIR/Julia_Scripts/run_LMCLUS_image.jl 10 $CURR_DIR/../subspace_datasets/real_datasets/image_datasets/lego efficientnet_avg_pool $CURR_DIR/../Results/real_datasets/image_datasets/lego/LMCLUS
+julia $CURR_DIR/Julia_scripts/run_LMCLUS_image.jl 10 $CURR_DIR/../subspace_datasets/real_datasets/image_datasets/lego vgg16_fc2 $CURR_DIR/../Results/real_datasets/image_datasets/lego/LMCLUS
+julia $CURR_DIR/Julia_scripts/run_LMCLUS_image.jl 10 $CURR_DIR/../subspace_datasets/real_datasets/image_datasets/lego vgg19_fc2 $CURR_DIR/../Results/real_datasets/image_datasets/lego/LMCLUS
+julia $CURR_DIR/Julia_scripts/run_LMCLUS_image.jl 10 $CURR_DIR/../subspace_datasets/real_datasets/image_datasets/lego resnet_avg_pool $CURR_DIR/../Results/real_datasets/image_datasets/lego/LMCLUS
+julia $CURR_DIR/Julia_scripts/run_LMCLUS_image.jl 10 $CURR_DIR/../subspace_datasets/real_datasets/image_datasets/lego xception_avg_pool $CURR_DIR/../Results/real_datasets/image_datasets/lego/LMCLUS
+julia $CURR_DIR/Julia_scripts/run_LMCLUS_image.jl 10 $CURR_DIR/../subspace_datasets/real_datasets/image_datasets/lego inception_avg_pool $CURR_DIR/../Results/real_datasets/image_datasets/lego/LMCLUS
+julia $CURR_DIR/Julia_scripts/run_LMCLUS_image.jl 10 $CURR_DIR/../subspace_datasets/real_datasets/image_datasets/lego efficientnet_avg_pool $CURR_DIR/../Results/real_datasets/image_datasets/lego/LMCLUS
 
 echo "LEGO dataset experiments finished!"
 
 echo "FRUITS dataset experiments started"
 
 # 2.1.3 fruits dataset
-julia $CURR_DIR/Julia_Scripts/run_LMCLUS_image.jl 10 $CURR_DIR/../subspace_datasets/real_datasets/image_datasets/fruits vgg16_fc2 $CURR_DIR/../Results/real_datasets/image_datasets/fruits/LMCLUS
-julia $CURR_DIR/Julia_Scripts/run_LMCLUS_image.jl 10 $CURR_DIR/../subspace_datasets/real_datasets/image_datasets/fruits vgg19_fc2 $CURR_DIR/../Results/real_datasets/image_datasets/fruits/LMCLUS
-julia $CURR_DIR/Julia_Scripts/run_LMCLUS_image.jl 10 $CURR_DIR/../subspace_datasets/real_datasets/image_datasets/fruits resnet_avg_pool $CURR_DIR/../Results/real_datasets/image_datasets/fruits/LMCLUS
-julia $CURR_DIR/Julia_Scripts/run_LMCLUS_image.jl 10 $CURR_DIR/../subspace_datasets/real_datasets/image_datasets/fruits xception_avg_pool $CURR_DIR/../Results/real_datasets/image_datasets/fruits/LMCLUS
-julia $CURR_DIR/Julia_Scripts/run_LMCLUS_image.jl 10 $CURR_DIR/../subspace_datasets/real_datasets/image_datasets/fruits inception_avg_pool $CURR_DIR/../Results/real_datasets/image_datasets/fruits/LMCLUS
-julia $CURR_DIR/Julia_Scripts/run_LMCLUS_image.jl 10 $CURR_DIR/../subspace_datasets/real_datasets/image_datasets/fruits efficientnet_avg_pool $CURR_DIR/../Results/real_datasets/image_datasets/fruits/LMCLUS
+julia $CURR_DIR/Julia_scripts/run_LMCLUS_image.jl 10 $CURR_DIR/../subspace_datasets/real_datasets/image_datasets/fruits vgg16_fc2 $CURR_DIR/../Results/real_datasets/image_datasets/fruits/LMCLUS
+julia $CURR_DIR/Julia_scripts/run_LMCLUS_image.jl 10 $CURR_DIR/../subspace_datasets/real_datasets/image_datasets/fruits vgg19_fc2 $CURR_DIR/../Results/real_datasets/image_datasets/fruits/LMCLUS
+julia $CURR_DIR/Julia_scripts/run_LMCLUS_image.jl 10 $CURR_DIR/../subspace_datasets/real_datasets/image_datasets/fruits resnet_avg_pool $CURR_DIR/../Results/real_datasets/image_datasets/fruits/LMCLUS
+julia $CURR_DIR/Julia_scripts/run_LMCLUS_image.jl 10 $CURR_DIR/../subspace_datasets/real_datasets/image_datasets/fruits xception_avg_pool $CURR_DIR/../Results/real_datasets/image_datasets/fruits/LMCLUS
+julia $CURR_DIR/Julia_scripts/run_LMCLUS_image.jl 10 $CURR_DIR/../subspace_datasets/real_datasets/image_datasets/fruits inception_avg_pool $CURR_DIR/../Results/real_datasets/image_datasets/fruits/LMCLUS
+julia $CURR_DIR/Julia_scripts/run_LMCLUS_image.jl 10 $CURR_DIR/../subspace_datasets/real_datasets/image_datasets/fruits efficientnet_avg_pool $CURR_DIR/../Results/real_datasets/image_datasets/fruits/LMCLUS
 
 echo "FRUITS dataset experiments finished!"
 
@@ -63,6 +63,6 @@ echo "FRUITS dataset experiments finished!"
 
 echo "ANURAN CALLS dataset experiment started..."
 
-julia run_LMCLUS_anuran.jl 100 $CURR_DIR/../subspace_datasets/real_datasets/anuran_calls_dataset ../../Result/real_dataset/anuran_calls_dataset/LMCLUS
+julia $CURR_DIR/Julia_scripts/run_LMCLUS_anuran.jl 100 $CURR_DIR/../subspace_datasets/real_datasets/anuran_calls_dataset $CURR_DIR/../Results/real_datasets/anuran_calls_dataset/LMCLUS
 
 echo "ANURAN CALLS dataset experiment finished!"
